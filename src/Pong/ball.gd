813e19f65d93022f2a60ba00e0f43cfe8a373d8d
Aminata Tengbe <a.l.tengbe@proton.me>
2026-05-28 10:00:37 +0100

    fix: add missing ball script and type

--- src/Pong/ball.gd
+++ src/Pong/ball.gd
@@ -1,1 +1,20 @@
-extends Area2D
+class_name PongBall extends Area2D
+
+@export var speed: FloatDoubler
+
+var velocity: Vector2
+
+func _process(delta: float) -> void:
+	position += velocity * delta * speed.value
+	
+func bounce(normal: Vector2) -> void:
+	velocity = velocity.bounce(normal)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	match body.name:
+		"Paddle":
+			bounce(Vector2.RIGHT)
+		_:
+			bounce(Vector2.UP)
+		
