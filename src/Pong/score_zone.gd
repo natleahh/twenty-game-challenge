089548c49bd76584c1ca78ec247df4affb5f80bb
Aminata Tengbe <a.l.tengbe@proton.me>
2026-05-28 12:03:15 +0100

    feat: add score tracking and display

--- src/Pong/score_zone.gd
+++ src/Pong/score_zone.gd
@@ -10,6 +10,6 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area2D) -> void:
-	var winner = Player.PLAYER_ONE if area.position.x < middle else Player.PLAYER_TWO
+	var winner = Player.PLAYER_ONE if area.position.x > middle else Player.PLAYER_TWO
 	point.emit(winner)
 	
