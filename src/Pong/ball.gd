c4c016e8e511a0f327bfaa323eaa7a21f2a599c8
Aminata Tengbe <a.l.tengbe@proton.me>
2026-05-28 12:02:36 +0100

    ref: move serving logic to ball

--- src/Pong/ball.gd
+++ src/Pong/ball.gd
@@ -3,12 +3,27 @@ class_name PongBall extends Area2D
 @export var speed: FloatDoubler
 
 var velocity: Vector2
+var serve_range: float = 0.2
+
 
 func _process(delta: float) -> void:
 	position += velocity * delta * speed.value
 	
 func bounce(normal: Vector2) -> void:
 	velocity = velocity.bounce(normal)
+
+func serve() -> void:
+	position = get_viewport_rect().get_center()
+	velocity = get_serve_vector()
+
+func get_serve_vector() -> Vector2:
+	var initial_arc: Vector2 = Vector2.from_angle(
+		randf_range(-PI * serve_range, PI * serve_range)
+	)
+	if randi() % 2:
+		return initial_arc
+	else:
+		return initial_arc * Vector2(-1, 1)
 
 func _on_body_entered(body: CollisionObject2D) -> void:
 	if body.collision_layer == 2:	
--- src/Pong/main.gd
+++ src/Pong/main.gd
@@ -6,25 +6,13 @@ extends Node2D
 @onready var player_1: Paddle = $Player1
 @onready var player_2: Paddle = $Player2
 
-var serve_range: float = 0.2
 
 func _ready() -> void:
-	handle_ball_serve()
+	pong_ball.serve()
 
 func _process(_delta: float) -> void:
 	handle_player_movement()
 
-func get_serve_vector() -> Vector2:
-	var initial_arc: Vector2 = Vector2.from_angle(
-		randf_range(-PI * serve_range, PI * serve_range)
-	)
-	if randi() % 2:
-		return initial_arc
-	else:
-		return initial_arc * Vector2(-1, 1)
-
-func handle_ball_serve() -> void:
-	pong_ball.velocity = get_serve_vector()
 
 func handle_player_movement() -> void:
 	player_1.direction = pong_controller.player_1_direction
