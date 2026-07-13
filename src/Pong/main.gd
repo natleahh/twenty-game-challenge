ad5ae559c5d32a54121222c021faee9739fde33c
natleahh <71596982+natleahh@users.noreply.github.com>
2026-07-13 18:18:37 +0100

    ref: make use of collision in phyics engine, add ball spawning
    Co-authored-by: Aminata Tengbe <a.l.tengbe@proton.me>

--- src/Pong/main.gd
+++ src/Pong/main.gd
@@ -1,29 +1,51 @@
 extends Node2D
 
-@onready var pong_ball: PongBall = $PongBall
-@onready var pong_controller: PongPlayerController = $PongPlayerController
-@onready var score_board: Control = $CanvasLayer/ScoreBoard
+const BALL: PackedScene = preload("res://src/Pong/ball/main.tscn")
 
-@onready var player_1: Paddle = $Player1
-@onready var player_2: Paddle = $Player2
+@export var pong_player_controller: PongPlayerController
+@export var player_1: Paddle
+@export var player_2: Paddle
 
-var score: Dictionary[ScoreZone.Player, int] = {
-	ScoreZone.Player.PLAYER_ONE: 0,
-	ScoreZone.Player.PLAYER_TWO: 0
+@export var ceiling: StaticBody2D
+@export var walls: StaticBody2D
+
+@export var score_board: ScoreBoard
+
+var score: Dictionary[int, int] = {
+	1: 0,
+	2: 0	
 }
 
 func _ready() -> void:
-	pong_ball.serve()
+	serve_ball()
 
 func _process(_delta: float) -> void:
 	handle_player_movement()
 
-
 func handle_player_movement() -> void:
-	player_1.direction = pong_controller.player_1_direction
-	player_2.direction = pong_controller.player_2_direction
+	player_1.direction = pong_player_controller.player_1_direction
+	player_2.direction = pong_player_controller.player_2_direction
 
-func _on_score_zone_point(winner: ScoreZone.Player) -> void:
-	pong_ball.serve()
-	score[winner] += 1
+func _on_ball_collision(body: Node, ball: RigidBody2D) -> void:
+	match body:
+		walls:
+			ball.queue_free()
+			serve_ball()
+			update_score(2 if ball.position.x < 0 else 1, 1)
+		Paddle:
+			ball.apply_central_impulse(ball.linear_velocity.bounce(Vector2.UP))
+			
+func serve_ball() -> void:
+	# new ball 
+	var ball: RigidBody2D  = BALL.instantiate()
+
+	# connect collision events
+	var _u: int = ball.body_entered.connect(_on_ball_collision.bind(ball))
+	ball.linear_velocity = Vector2.from_angle(randf() * 2 * PI) * 300
+	
+	# add to scene
+	call_deferred("add_child", ball)
+
+func update_score(winner: int, update: int) -> void:
+	score[winner] += update
 	score_board.update_score(score)
